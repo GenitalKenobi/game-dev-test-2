f30c0fedb2d5696c168ca0ae502d1c0eb32ac35a
GenitalKenobi <prashantss321@gmail.com>
2026-02-16 00:14:12 +0530

    good stopping point for this test

--- app.js
+++ app.js
@@ -96,75 +96,136 @@ class Character {
     
         ctx.fillStyle = this.color;
         ctx.strokeStyle = this.color;
-        ctx.lineWidth = 8;
         ctx.lineCap = 'round';
+        ctx.lineJoin = 'round';
     
         // Head
         ctx.beginPath();
         ctx.arc(0, -this.height / 2.5, this.width / 3, 0, Math.PI * 2);
         ctx.fill();
+
+        // Eye
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(this.width / 5, -this.height / 2.5 - this.width / 12, this.width / 10, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.fillStyle = 'black';
+        ctx.beginPath();
+        ctx.arc(this.width / 5 + this.width / 20, -this.height / 2.5 - this.width / 12, this.width / 25, 0, Math.PI * 2);
+        ctx.fill();
+
+        // Restore color
+        ctx.fillStyle = this.color;
+        ctx.strokeStyle = this.color;
     
         // Body
+        ctx.lineWidth = 20;
         ctx.beginPath();
         ctx.moveTo(0, -this.height / 4);
-        ctx.lineTo(0, this.height / 4);
+        ctx.lineTo(0, this.height / 5);
         ctx.stroke();
     
+        // Arms and Legs
+        ctx.lineWidth = 12;
+        const upperArmLength = this.width / 2;
+        const lowerArmLength = this.width / 2;
+        const upperLegLength = this.height / 4;
+        const lowerLegLength = this.height / 3;
+        
+        const shoulderY = -this.height / 4.5;
+        const hipY = this.height / 5;
+
         // Arms
         if (this.isAttacking && this.attackType === 'punch') {
-            // Punching arm
+            const animationSpeed = 20;
+            const punchProgress = this.attackFrame > animationSpeed / 2 ? (animationSpeed - this.attackFrame) / (animationSpeed / 2) : this.attackFrame / (animationSpeed / 2);
+            
+            // Back arm (Guard)
             ctx.beginPath();
-            ctx.moveTo(0, 0);
-            ctx.lineTo(this.width / 1.5, -this.height / 10);
+            ctx.moveTo(0, shoulderY);
+            ctx.lineTo(-upperArmLength * 0.5, shoulderY + lowerArmLength * 0.5);
+            ctx.lineTo(0, shoulderY + lowerArmLength * 0.2);
             ctx.stroke();
-            // Other arm
+
+            // Punching arm
+            // Move shoulder forward
+            const shoulderForward = 10 * punchProgress;
+            const elbowX = upperArmLength + shoulderForward;
+            const elbowY = shoulderY;
+            const handX = elbowX + lowerArmLength * punchProgress;
+            const handY = elbowY;
             ctx.beginPath();
-            ctx.moveTo(0, 0);
-            ctx.lineTo(-this.width / 2, this.height / 10);
+            ctx.moveTo(shoulderForward, shoulderY);
+            ctx.lineTo(elbowX, elbowY);
+            ctx.lineTo(handX, handY);
             ctx.stroke();
         } else {
             // Idle arms
             ctx.beginPath();
-            ctx.moveTo(0, 0);
-            ctx.lineTo(-this.width / 2.5, this.height / 5);
+            ctx.moveTo(0, shoulderY);
+            ctx.lineTo(-upperArmLength * 0.3, shoulderY + upperArmLength * 0.8);
+            ctx.lineTo(upperArmLength * 0.2, shoulderY + upperArmLength * 0.8 + lowerArmLength * 0.5);
             ctx.stroke();
-            ctx.moveTo(0, 0);
-            ctx.lineTo(this.width / 2.5, this.height / 5);
+            ctx.beginPath();
+            ctx.moveTo(0, shoulderY);
+            ctx.lineTo(upperArmLength * 0.3, shoulderY + upperArmLength * 0.8);
+            ctx.lineTo(-upperArmLength * 0.2, shoulderY + upperArmLength * 0.8 + lowerArmLength * 0.5);
             ctx.stroke();
         }
     
         // Legs
         if (this.isAttacking && this.attackType === 'kick') {
-            // Kicking leg
-            ctx.beginPath();
-            ctx.moveTo(0, this.height / 4);
-            ctx.lineTo(this.width, this.height / 4);
-            ctx.stroke();
+            const animationSpeed = 20;
+            const kickProgress = this.attackFrame > animationSpeed / 2 ? (animationSpeed - this.attackFrame) / (animationSpeed / 2) : this.attackFrame / (animationSpeed / 2);
+            
             // Back leg
             ctx.beginPath();
-            ctx.moveTo(0, this.height / 4);
-            ctx.lineTo(-this.width / 2, this.height / 2);
+            ctx.moveTo(0, hipY);
+            ctx.lineTo(-upperLegLength * 0.2, hipY + upperLegLength);
+            ctx.lineTo(-upperLegLength * 0.2, hipY + upperLegLength + lowerLegLength);
+            ctx.stroke();
+
+            // Kicking leg
+            const kneeX = upperLegLength;
+            const kneeY = hipY;
+            const footX = kneeX + lowerLegLength * kickProgress;
+            const footY = kneeY;
+            
+            ctx.beginPath();
+            ctx.moveTo(0, hipY);
+            ctx.lineTo(kneeX * kickProgress, kneeY - (upperLegLength * 0.2 * kickProgress));
+            ctx.lineTo(footX, footY - (upperLegLength * 0.2 * kickProgress));
             ctx.stroke();
         } else if (this.isWalking && !this.isJumping) {
             const legAngle = Math.sin(this.walkFrame * 0.3) * 0.5;
             // Leg 1
+            const knee1X = upperLegLength * Math.sin(legAngle);
+            const knee1Y = hipY + upperLegLength * Math.cos(legAngle);
             ctx.beginPath();
-            ctx.moveTo(0, this.height / 4);
-            ctx.lineTo(this.width / 2 * Math.cos(legAngle), this.height / 4 + this.height / 3 * Math.sin(legAngle));
+            ctx.moveTo(0, hipY);
+            ctx.lineTo(knee1X, knee1Y);
+            ctx.lineTo(knee1X - lowerLegLength * Math.sin(legAngle) * 0.5, knee1Y + lowerLegLength * Math.cos(legAngle));
             ctx.stroke();
             // Leg 2
+            const legAngle2 = Math.sin(this.walkFrame * 0.3 + Math.PI) * 0.5;
+            const knee2X = upperLegLength * Math.sin(legAngle2);
+            const knee2Y = hipY + upperLegLength * Math.cos(legAngle2);
             ctx.beginPath();
-            ctx.moveTo(0, this.height / 4);
-            ctx.lineTo(this.width / 2 * Math.cos(legAngle + Math.PI), this.height / 4 + this.height / 3 * Math.sin(legAngle + Math.PI));
+            ctx.moveTo(0, hipY);
+            ctx.lineTo(knee2X, knee2Y);
+            ctx.lineTo(knee2X - lowerLegLength * Math.sin(legAngle2) * 0.5, knee2Y + lowerLegLength * Math.cos(legAngle2));
             ctx.stroke();
         } else {
             // Idle legs
             ctx.beginPath();
-            ctx.moveTo(0, this.height / 4);
-            ctx.lineTo(-this.width / 3, this.height / 2);
+            ctx.moveTo(0, hipY);
+            ctx.lineTo(-upperLegLength * 0.3, hipY + upperLegLength);
+            ctx.lineTo(-upperLegLength * 0.3, hipY + upperLegLength + lowerLegLength);
             ctx.stroke();
-            ctx.moveTo(0, this.height / 4);
-            ctx.lineTo(this.width / 3, this.height / 2);
+            ctx.beginPath();
+            ctx.moveTo(0, hipY);
+            ctx.lineTo(upperLegLength * 0.3, hipY + upperLegLength);
+            ctx.lineTo(upperLegLength * 0.3, hipY + upperLegLength + lowerLegLength);
             ctx.stroke();
         }
     
@@ -212,6 +273,12 @@ class Character {
 
         if (this.isAttacking) {
             this.attackFrame++;
+            const animationSpeed = 20;
+            if (this.attackFrame > animationSpeed) {
+                this.isAttacking = false;
+                this.attackType = null;
+                this.attackFrame = 0;
+            }
         }
 
 
@@ -269,7 +336,11 @@ class Character {
         this.attackFrame = 0;
         const attackWidth = type === 'punch' ? 50 : 70;
         const attackHeight = type === 'punch' ? 20 : 30;
-        const attackY = type === 'punch' ? this.y : this.y + this.height / 2;
+        
+        // Align hitbox with visual limbs
+        const shoulderY = this.y + this.height / 2 - this.height / 4.5;
+        const hipY = this.y + this.height / 2 + this.height / 5;
+        const attackY = type === 'punch' ? shoulderY - attackHeight / 2 : hipY - attackHeight / 2;
         
         if(this.direction === 'right'){
             this.attackBox = {
@@ -286,12 +357,6 @@ class Character {
                 height: attackHeight
             };
         }
-
-
-        setTimeout(() => {
-            this.isAttacking = false;
-            this.attackType = null;
-        }, 600);
     }
 }
 
